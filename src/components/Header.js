5e24dd052c6df33afb9e1815144ea12ce886efb8
harish242 <markollh242@gmail.com>
2023-06-09 11:51:58 +0530

    xcf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const Header = () => {}
+const Header = () => {
+    return(
+        <div id='header-component'>
+            <h1>Newton School</h1>
+        </div>
+        
+    )
+}
 
 
 export default Header;
